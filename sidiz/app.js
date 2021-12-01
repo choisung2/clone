cd9679bb693d9f35165d18be09662b445f5ad2c8
choisung2 <c0817d@gmail.com>
2021-12-01 23:28:41 +0900

    Update app.js

--- sidiz/app.js
+++ sidiz/app.js
@@ -1,12 +1,32 @@
-function slideOn () {
-    const slide = document.querySelector('.slide-wrap');
-    const item = slide.getElementsByClassName('slide-contents');
-    const firstSlide = item[0];
+// function slideOn () {
+//     const slide = document.querySelector('.slide-wrap');
+//     const item = slide.getElementsByClassName('slide-contents');
+//     const firstSlide = item[0];
 
-    firstSlide.classList.add('onSlide');
-}
+//     firstSlide.classList.add('onSlide');
+// }
 
-slideOn();
+// slideOn();
+
+// const second = setInterval(slideGo, 5000);
+
+// function slideGo() {
+//     const currentItem = document.querySelector('.onSlide');
+//     currentItem.classList.remove('onSlide');
+
+//     if (!currentItem.nextElementSibling) {
+//         item[0].classList.add('onSlide');
+//     } else {
+//         currentItem.nextElementSibling.classList.add('onSlide');
+//     }
+// }
+
+//준홍님이 고쳐주신거! 뜯어보자!!
+
+const slide = document.querySelector('.slide-wrap');
+const item = slide.querySelectorAll('.slide-contents');
+const firstSlide = item[0];
+firstSlide.classList.add('onSlide');
 
 const second = setInterval(slideGo, 5000);
 
