2cffbd8fb11de7498a6bcda56b23f2466a75ba8d
choisung2 <c0817d@gmail.com>
2022-01-07 01:23:09 +0900

    Add play the slide when click the button

--- sidiz/app.js
+++ sidiz/app.js
@@ -1,97 +1,44 @@
-// function slideOn () {
-//     const slide = document.querySelector('.slide-wrap');
-//     const item = slide.getElementsByClassName('slide-contents');
-//     const firstSlide = item[0];
+const slideItem = document.querySelectorAll('.slide-contents');
+let sliderTimer = setInterval(slideGo, 5000);
+let currentIndex = 0;
 
-//     firstSlide.classList.add('onSlide');
-// }
-
-// slideOn();
-
-// const second = setInterval(slideGo, 5000);
-
-// function slideGo() {
-//     const currentItem = document.querySelector('.onSlide');
-//     currentItem.classList.remove('onSlide');
-
-//     if (!currentItem.nextElementSibling) {
-//         item[0].classList.add('onSlide');
-//     } else {
-//         currentItem.nextElementSibling.classList.add('onSlide');
-//     }
-// }
-
-//준홍님이 고쳐주신거! 뜯어보자!!
-
-const slide = document.querySelector('.slide-wrap');
-const item = slide.querySelectorAll('.slide-contents');
-const firstSlide = item[0];
-firstSlide.classList.add('onSlide');
-
-const second = setInterval(slideGo, 5000);
+window.onload = () => {
+  slideItem[currentIndex].classList.add('onSlide');
+}
 
 function slideGo() {
     const currentItem = document.querySelector('.onSlide');
     currentItem.classList.remove('onSlide');
 
     if (!currentItem.nextElementSibling) {
-        item[0].classList.add('onSlide');
+        slideItem[0].classList.add('onSlide');
     } else {
         currentItem.nextElementSibling.classList.add('onSlide');
     }
 }
 
-const pre = document.querySelector('.slide-btn-pre');
-const next = document.querySelector('.slide-btn-next');
+function slidePrev() {
+    const currentItem = document.querySelector('.onSlide');
+    currentItem.classList.remove('onSlide');
 
-// var slideIndex = 0; //slide index
+    if (!currentItem.previousElementSibling) {
+        slideItem[4].classList.add('onSlide');
+    } else {
+        currentItem.previousElementSibling.classList.add('onSlide');
+    }
+}
 
-// // HTML 로드가 끝난 후 동작
-// window.onload=function(){
-//   showSlides(slideIndex);
+const prevBtn = document.querySelector('.slide-btn-pre');
+const nextBtn = document.querySelector('.slide-btn-next');
 
-//   // Auto Move Slide
-//   var sec = 3000;
-//   setInterval(function(){
-//     slideIndex++;
-//     showSlides(slideIndex);
+prevBtn.addEventListener('click', () => {
+    slidePrev()
+    clearInterval(sliderTimer);
+    sliderTimer = setInterval(slideGo, 5000);
+})
 
-//   }, sec);
-// }
-
-
-// // Next/previous controls
-// function moveSlides(n) {
-//   slideIndex = slideIndex + n
-//   showSlides(slideIndex);
-// }
-
-// // Thumbnail image controls
-// function currentSlide(n) {
-//   slideIndex = n;
-//   showSlides(slideIndex);
-// }
-
-// function showSlides(n) {
-
-//   var slides = document.getElementsByClassName("mySlides");
-//   var dots = document.getElementsByClassName("dot");
-//   var size = slides.length;
-
-//   if ((n+1) > size) {
-//     slideIndex = 0; n = 0;
-//   }else if (n < 0) {
-//     slideIndex = (size-1);
-//     n = (size-1);
-//   }
-
-//   for (i = 0; i < slides.length; i++) {
-//       slides[i].style.display = "none";
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//       dots[i].className = dots[i].className.replace(" active", "");
-//   }
-
-//   slides[n].style.display = "block";
-//   dots[n].className += " active";
-// }
+nextBtn.addEventListener('click', () => {
+    slideGo()
+    clearInterval(sliderTimer);
+    sliderTimer = setInterval(slideGo, 5000);
+})
